b194d703619aef45ad4766d7900359d38895eb99
EdwinVBN <Katuku.edwin@gmail.com>
2023-05-26 09:10:07 +0200

    Update script.js

--- liveleopardserver/testV6.1/script.js
+++ liveleopardserver/testV6.1/script.js
@@ -1,19 +1,24 @@
-// fetch('http://192.168.0.100:8000/api/set')
-//     .then((r) => {return r.json();})
-//     .then((data) => {console.log(data)})
-const state = 'toggle';
-const button_topic = document.querySelector('#toggle');
-const topic = button_topic.value;
+function buttOn(){
+    let state = 'toggle';
+    let topic = "woonkamer/schemerlamp";
 
-payload = {
-    'topic': topic,
-    'feature': {'state': state } };
-const options = {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify( payload )
-};
+    fetch('http://192.168.0.100:8000/api/set', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            'topic': topic,
+            'feature': {'state': state } 
+        })
+    })
 
-fetch('http://192.168.0.100:8000/api/set', options)
+    .then(res => res.json())
+    .then(data => console.log(data))
+
+    document.querySelector('#toggle').onclick = (e) =>  {
+        const state = 'toggle';
+        const button_topic = document.querySelector('#toggle');
+        const topic = button_topic.value;
+    }
+}
