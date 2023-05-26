8af443108cfeb84f3f44ab18770ade96188299aa
EdwinVBN <Katuku.edwin@gmail.com>
2023-05-26 12:04:16 +0200

    css

--- liveleopardserver/testV6.1/script.js
+++ liveleopardserver/testV6.1/script.js
@@ -17,3 +17,26 @@ document.getElementById('toggle').onclick = (e) =>  {
     fetch('http://192.168.0.100:8000/api/set', options)
 };
 
+document.getElementById('color').onclick = (e) =>  {
+    // const state = 'toggle';
+    const button_topic = document.getElementById('color');
+    const topic = button_topic.value;
+
+    payload = {
+        'topic': topic,
+        'feature': {'state': state } };
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify( payload )
+    };
+    
+    fetch('http://192.168.0.100:8000/api/set', options)
+};
+
+// 233.2}
+// woonkamer/schemerlamp: {"brightness":50,"color":{"x":0.7006,"y":0.2993},
+// "color_mode":"xy","color_temp":160,"level_config":{"on_level":"previous"},"linkquality":255,
+// "state":"ON","update":{"installed_version":587806257,"latest_version":587806257,
