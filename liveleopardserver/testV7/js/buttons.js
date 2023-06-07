8e1f2ffaf4d54f0f73aca48ac092fe2ff60238b0
EdwinVBN <Katuku.edwin@gmail.com>
2023-06-07 11:04:00 +0200

    new

--- liveleopardserver/testV7/js/buttons.js
+++ liveleopardserver/testV7/js/buttons.js
@@ -19,7 +19,7 @@ class DeviceList {
     constructor() {
         this.devices = [];
 
-        fetch("js\device.json")
+        fetch("js/device.json")
             .then((res) => res.json())
             .then((items) => this.parse(items));
     }
@@ -38,6 +38,9 @@ class DeviceList {
         const container = document.getElementById("devices");
         container.innerHTML = "";
 
+        for(let device of this.devices){   
+        container.innerHTML += `${device.name} <br>`;
+        }
 
     }
 }
